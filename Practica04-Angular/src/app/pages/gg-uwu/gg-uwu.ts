b8251623c6bbe1bcdb1137ef300079935f386962
estebanbaca-code <estebanbaca@micorreo.upp.edu.mx>
2026-01-22 15:32:38 -0600

    Create gg-uwu.ts

---
+++ Practica04-Angular/src/app/pages/gg-uwu/gg-uwu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gg-uwu',
+  imports: [],
+  templateUrl: './gg-uwu.html',
+  styleUrl: './gg-uwu.css',
+})
+export class GgUWU {
+
+}
