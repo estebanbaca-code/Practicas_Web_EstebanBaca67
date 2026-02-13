fc25cdeb0f753271bf10cf2c9d76815299c5e621
estebanbaca-code <estebanbaca@micorreo.upp.edu.mx>
2026-02-13 11:53:40 -0600

    Create esteban.ts

---
+++ Practica06-js-modal/src/app/esteban/esteban.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esteban',
+  imports: [],
+  templateUrl: './esteban.html',
+  styleUrl: './esteban.css',
+})
+export class Esteban {
+
+}
